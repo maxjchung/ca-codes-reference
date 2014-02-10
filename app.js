d0b7849f49b405f82bad6350709c691437a7b7c1
maxjchung <max@maxjchung.com>
2014-02-09 16:35:04 -0800

    Revealing module pattern for app.js.

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-alert('hai');
+var CodeBrowser = (function () {
+
+	// Private members
+	function getAllCategories() {
+		console.log("called getAllCategories()")
+		return $.ajax('http://verbar.herokuapp.com/rest/json?select=all');
+	}
+
+	// Public interface
+	return {
+		getAllCategories: getAllCategories
+	}
+
+})();
+
+CodeBrowser.getAllCategories();
