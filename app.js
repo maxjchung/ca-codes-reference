174c1bbcabc670919a3da46c472edd42c0e6ebc3
maxjchung <max@maxjchung.com>
2014-02-16 14:59:44 -0800

    Generate links from category results.

--- app.js
+++ app.js
@@ -15,17 +15,24 @@ var CodeBrowser = (function () {
 				console.log(response);
 				var categories = getCategories(response);
 
-				// TODO: Update the UI with the categories
-
 				var i = 0;
 				for (i ; i < categories.length; i++) {
-					$('.results').append(categories[i].title);
+					$('.results').append(getCategoryLink(categories[i].title, categories[i].fullFacet));
 					$('.results').append("<br/>");
 				}
 			}
 		});
 	}
 
+
+	function getCategoryLink(text, category) {
+		var url = 'http://verbar.herokuapp.com/rest/json' + '?path=' + category;
+
+		return $('<a>',{
+		    text: text,
+		    href: url
+		});
+	}
 
 	function getCategories(jsonResponse) {
 		var codes = jsonResponse.codesAvailable;
