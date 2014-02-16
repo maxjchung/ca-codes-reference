e6f7dc586068b77fb02e09b7d0434d56d2f27850
maxjchung <max@maxjchung.com>
2014-02-16 15:27:34 -0800

    Load subcategories when clicking on links.

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ var CodeBrowser = (function () {
 			url: 'http://verbar.herokuapp.com/rest/json',
 			success: function(response) {
 
-				console.log(response);
 				var categories = getCategories(response);
 
 				var i = 0;
@@ -20,6 +19,7 @@ var CodeBrowser = (function () {
 					$('.results').append(getCategoryLink(categories[i].title, categories[i].fullFacet));
 					$('.results').append("<br/>");
 				}
+				
 			}
 		});
 	}
@@ -28,10 +28,12 @@ var CodeBrowser = (function () {
 	function getCategoryLink(text, category) {
 		var url = 'http://verbar.herokuapp.com/rest/json' + '?path=' + category;
 
+
 		return $('<a>',{
 		    text: text,
-		    href: url
-		});
+		    href: '#',
+		    click: updateResultsList
+		}).data('categoryPath', category);
 	}
 
 	function getCategories(jsonResponse) {
@@ -39,6 +41,36 @@ var CodeBrowser = (function () {
 		return codes;
 	}
 
+	function getSubcategoriesFromJson(jsonResponse) {
+		var codes = jsonResponse.subcodeList;
+		return codes;
+	}
+
+	function updateResultsList() {
+
+		var category = $(this).data('categoryPath');
+		var url = 'http://verbar.herokuapp.com/rest/json' + '?path=' + category;
+
+		$('.results').empty();
+		$('.results').append("Loading...");
+
+
+		$.ajax({
+			url: url,
+			success: function(response) {
+
+				$('.results').empty();
+
+				var subcategories = getSubcategoriesFromJson(response);
+
+				var i = 0;
+				for (i ; i < subcategories.length; i++) {
+					$('.results').append(getCategoryLink(subcategories[i].title, subcategories[i].fullFacet));
+					$('.results').append("<br/>");
+				}
+			}
+		});
+	}
 
 	// Public interface
 	return {
